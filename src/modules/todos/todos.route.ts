66e4f9c8d114f6d4d4527b4d96563ddfeddca0e5
lidarbtc <lidarbtc@protonmail.com>
2025-11-15 14:57:27 +0900

    o

--- src/modules/todos/todos.route.ts
+++
@@ -1,8 +0,0 @@
-// Route builders with dynamic parameters
-const todosRoutes = {
-	list: "/dashboard/todos",
-	new: "/dashboard/todos/new",
-	edit: (id: string | number) => `/dashboard/todos/${id}/edit`,
-} as const;
-
-export default todosRoutes;
